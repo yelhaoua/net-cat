d5b37c4ff6c498d3324c2be51d889bd99bf37aae
yelhaoua <yacobelhaouarii@gmail.com>
2026-01-07 17:30:54 +0100

    Understanding the tcp and chanles

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net"
+
+	"net-cat/handlers"
+)
+
 func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	fmt.Println("Chat server running on :8080")
+
+	go handlers.Broadcaster()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handlers.HandleClient(conn)
+	}
 }
